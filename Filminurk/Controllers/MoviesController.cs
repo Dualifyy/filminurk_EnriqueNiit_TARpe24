c3228c011b0561f9fc29c259de9147065288e9e1
Opilane TTHK <opilane@tthk.ee>
2025-10-09 11:12:21 +0300

    added view properties into view
    -dualfify yoyyyyooo

--- Filminurk/Controllers/MoviesController.cs
+++ Filminurk/Controllers/MoviesController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Filminurk.Data;
+using Filminurk.Models.Movies;
 
 namespace Filminurk.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly FilminurkTARpe24Context _context;
+        public MoviesController (FilminurkTARpe24Context context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Movies.Select(x => new MoviesIndexViewModel
+            {
+                ID = x.ID,
+                Title = x.Title,
+                FirstPublished = x.FirstPublished,
+                CurrentRating = x.CurrentRating,
+                UserRating = x.UserRating,
+                MovieLength = x.MovieLength,
+            });
+            return View(result);
         }
     }
 }
--- Filminurk/Models/Movies/MoviesIndexViewModel.cs
+++ Filminurk/Models/Movies/MoviesIndexViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class MoviesIndexViewModel
     {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public decimal CurrentRating { get; set; }
+        //public List<UserComment>? Reviews { get; set; }
+
+        /*2 omal valikul andmetüüpi yo*/
+
+        public double UserRating { get; set; }
+        public int MovieLength { get; set; }
     }
 }
