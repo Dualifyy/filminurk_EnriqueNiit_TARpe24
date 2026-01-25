973e612bf3ec61b56d61eaf612ff17b047e9915b
Dualify <niitenriquefederico@gmail.com>
2026-01-25 11:00:48 +0200

    AddPassword in AccountsController

--- Filminurk/Controllers/AccountsController.cs
+++ Filminurk/Controllers/AccountsController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Filminurk.Core.ServiceInterface;
+using Filminurk.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filminurk.Controllers
 {
     public class AccountsController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly FilminurkTARpe24Context _context;
+        private readonly IEmailsServices _emailsServices;
+
+        public AccountsController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            FilminurkTARpe24Context context
+            )
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+            if (userHasPassword)
+            {
+                return RedirectToAction("ChangePassword");
+            }
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                if (result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return View();
+
+                }
+                await _signInManager.RefreshSignInAsync(user);
+                return View("AddPasswordConfirmation");
+            }
+            return View(model);
         }
     }
 }
