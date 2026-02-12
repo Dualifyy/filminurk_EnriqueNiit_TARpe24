bdf5aa8c2a75c822c5c093eff70964c84bdd2077
Dualify <niitenriquefederico@gmail.com>
2026-02-12 19:28:38 +0200

    controller work

--- Filminurk/Controllers/SignalRController.cs
+++ Filminurk/Controllers/SignalRController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Filminurk.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filminurk.Controllers
 {
     public class SignalRController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public SignalRController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) 
+        { 
+            _userManager = userManager; 
+            _signInManager = signInManager; 
+        }
         public IActionResult Index()
         {
             return View();
