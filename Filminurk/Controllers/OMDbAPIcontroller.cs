25b46b8918e141a0ec14a37227621ff9b247bfb4
Dualify <niitenriquefederico@gmail.com>
2026-02-13 12:29:25 +0200

    Start of OMDbAPI 9.5

---
+++ Filminurk/Controllers/OMDbAPIcontroller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class OMDbAPIcontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            //bruhhh
+            return View();
+        }
+    }
+}
